45212b276311762e795ad552e0f37feafa26e4b6
nicknlowe <nick.lowe@my.jcu.edu.au>
2024-09-29 11:26:28 +1000

    Refactor password print program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -4,9 +4,23 @@ Program that prints asterisks based on password length
 
 MINIMUM_LENGTH_THRESHOLD = 6
 
-password = input("Password: ")
-while len(password) < MINIMUM_LENGTH_THRESHOLD:
-    print("Password must be at least 6 characters!")
+
+def main():
+    password = get_password()
+    print_asterisks(password)
+
+
+def print_asterisks(password):
+    for i in range(len(password)):
+        print("*", end="")
+
+
+def get_password():
     password = input("Password: ")
-for i in range(len(password)):
-    print("*", end="")
+    while len(password) < MINIMUM_LENGTH_THRESHOLD:
+        print("Password must be at least 6 characters!")
+        password = input("Password: ")
+    return password
+
+
+main()
