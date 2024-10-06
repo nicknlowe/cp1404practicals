7b96fbb9592e1b6686cfe85607657b83bb68beb9
nicknlowe <nick.lowe@my.jcu.edu.au>
2024-10-06 21:52:48 +1000

    Add random integer program

---
+++ prac_03/randoms.py
@@ -0,0 +1,16 @@
+# Line 1:
+# I saw 7 for line 1
+# The smallest number I could have seen was 5 and the largest would be 20
+
+# Line 2:
+# I saw 9 for line 2
+# The smallest number I could have seen was 3 and the largest would be 9
+# Line 2 could not have produced a 4 due to the step parameter only allowing for increments of 2
+
+# Line 3:
+# I saw 4.875049255442175 for line 3
+# The smallest number I could have seen was 2.5 and the largest would be 5.5
+
+import random
+
+print(random.randint(1, 100))
