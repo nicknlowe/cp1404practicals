cafc06e35852feaf00e347a5e5516442ebead814
Nick Lowe <nick.lowe@my.jcu.edu.au>
2024-10-20 11:52:24 +1000

    Change error handling method to use exceptions

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -1,7 +1,6 @@
 """
 CP1404/CP5632 Practical
 State names in a dictionary
-File needs reformatting
 """
 
 STATE_CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
@@ -10,11 +9,11 @@ print(STATE_CODE_TO_NAME)
 
 state_code = input("Enter short state: ").upper()
 while state_code != "":
-    if state_code in STATE_CODE_TO_NAME:
+    try:
         print(state_code, "is", STATE_CODE_TO_NAME[state_code])
-    else:
+    except KeyError:
         print("Invalid short state")
     state_code = input("Enter short state: ").upper()
 
-for states in STATE_CODE_TO_NAME:
-    print(f"{states:<3} is {STATE_CODE_TO_NAME[states]}")
+for state in STATE_CODE_TO_NAME:
+    print(f"{state:<3} is {STATE_CODE_TO_NAME[state]}")
