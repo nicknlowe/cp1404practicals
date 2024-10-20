fe24673fbe5a66955f3b7ca36e6c9661e7685b29
Nick Lowe <nick.lowe@my.jcu.edu.au>
2024-10-20 12:20:06 +1000

    Add word occurrences program

---
+++ prac_05/word_occurrences.py
@@ -0,0 +1,19 @@
+"""
+CP1404/CP5632 Practical
+Count the number of word occurrences in a string
+"""
+
+word_to_count = {}
+text = input("Text: ")
+words = text.split()
+for word in words:
+    try:
+        word_to_count[word] += 1
+    except KeyError:
+        word_to_count[word] = 1
+
+words = list(word_to_count.keys())
+words.sort()
+
+for word in words:
+    print(f"{word:{max((len(word) for word in words))}} : {word_to_count[word]}")
