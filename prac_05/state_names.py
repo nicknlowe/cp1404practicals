e060e08adb89ab980c17638721c8ce450a73497c
Nick Lowe <nick.lowe@my.jcu.edu.au>
2024-10-20 12:03:17 +1000

    Fix formatting error in dictionary

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -3,8 +3,9 @@ CP1404/CP5632 Practical
 State names in a dictionary
 """
 
-STATE_CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
-                "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania", "SA": "South Australia"}
+STATE_CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory",
+                      "WA": "Western Australia", "ACT": "Australian Capital Territory", "VIC": "Victoria",
+                      "TAS": "Tasmania", "SA": "South Australia"}
 print(STATE_CODE_TO_NAME)
 
 state_code = input("Enter short state: ").upper()
