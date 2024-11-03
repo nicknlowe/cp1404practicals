9474a89703eb089e0eb6295ce3b5137af153ca3a
Nick Lowe <nick.lowe@my.jcu.edu.au>
2024-11-03 11:52:38 +1000

    Add programming language class program

---
+++ prac_06/languages.py
@@ -0,0 +1,18 @@
+"""
+CP1404/CP5632 Practical
+Client code to use the ProgrammingLanguage class
+"""
+
+from prac_06.programming_language import ProgrammingLanguage
+
+python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+
+languages = [python, ruby, visual_basic]
+print(python)
+
+print("The dynamically typed languages are:")
+for language in languages:
+    if language.is_dynamic():
+        print(language.name)
