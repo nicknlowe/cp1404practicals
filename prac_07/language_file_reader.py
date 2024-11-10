c447e13daff2a8c5352a8c6c903a68a97347a38c
nicknlowe <nick.lowe@my.jcu.edu.au>
2024-11-10 18:37:22 +1000

    Add pointer arithmetic instance

--- prac_07/language_file_reader.py
+++ prac_07/language_file_reader.py
@@ -28,7 +28,9 @@ def main():
         reflection = parts[2] == "Yes"
         # Construct a ProgrammingLanguage object using the elements
         # year should be an int
-        language = ProgrammingLanguage(parts[0], parts[1], reflection, int(parts[3]))
+        # Pointer Arithmetic is stored as a string (Yes/No) and we want a Boolean
+        pointer_arithmetic = parts[4] == "Yes"
+        language = ProgrammingLanguage(parts[0], parts[1], reflection, int(parts[3]), pointer_arithmetic)
         # Add the language we've just constructed to the list
         languages.append(language)
     # Close the file as soon as we've finished reading it
