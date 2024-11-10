e239a258c633d5be5ec3a9a26f1a6017f60b6cb1
nicknlowe <nick.lowe@my.jcu.edu.au>
2024-11-10 19:00:36 +1000

    Add read file function

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -11,14 +11,19 @@ FILENAME = "guitars.csv"
 def main():
     """Read and display guitars from oldest to youngest."""
     guitars = []
-    with open(FILENAME, 'r', encoding='utf-8-sig') as in_file:
+    read_file(FILENAME, guitars)
+    guitars.sort()
+    for guitar in guitars:
+        print(guitar)
+
+
+def read_file(filename, guitars):
+    """Read guitar data from a CSV file."""
+    with open(filename, 'r', encoding='utf-8-sig') as in_file:
         for line in in_file:
             parts = line.strip().split(',')
             guitar = Guitar(parts[0], int(parts[1]), float(parts[2]))
             guitars.append(guitar)
-    guitars.sort()
-    for guitar in guitars:
-        print(guitar)
 
 
 main()
