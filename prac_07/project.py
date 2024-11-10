341a5159efdaca2a2d943e96cecd87d6b27c303a
nicknlowe <nick.lowe@my.jcu.edu.au>
2024-11-10 19:45:29 +1000

    Add project class

---
+++ prac_07/project.py
@@ -0,0 +1,21 @@
+"""
+CP1404/CP5632 Practical
+Project class
+"""
+
+
+class Project:
+    """Represent information about a Project."""
+
+    def __init__(self, name, start_date, priority, cost_estimate, completion_percentage):
+        """Construct a Project instance."""
+        self.name = name
+        self.start_date = start_date
+        self.priority = priority
+        self.cost_estimate = cost_estimate
+        self.completion_percentage = completion_percentage
+
+    def __str__(self):
+        """Returns a string of a Project instance."""
+        return (f"{self.name}, start: {self.start_date}, priority {self.priority}, estimate: ${self.cost_estimate:.2f}"
+                f", completion: {self.completion_percentage}%")
