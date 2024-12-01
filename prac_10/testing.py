29940f82cb3bb54879a78dd8ba02c165fad42d10
Nick Lowe <nick.lowe@my.jcu.edu.au>
2024-12-01 18:27:15 +1000

    Add phrase to sentence formatting function

--- prac_10/testing.py
+++ prac_10/testing.py
@@ -45,19 +45,22 @@ def run_tests():
     assert car.fuel == 0
 
 
+def phrase_to_sentence(phrase):
+    """
+    Format a phrase into a sentence, starting with a capital and ending with a single full stop.
+    >>> phrase_to_sentence("hello")
+    'Hello.'
+    >>> phrase_to_sentence("It is an ex parrot.")
+    'It is an ex parrot.'
+    >>> phrase_to_sentence("wow, that was such a good song.")
+    'Wow, that was such a good song.'
+    """
+    sentence = phrase.capitalize()
+    if not sentence[-1] == '.':
+        sentence = f"{sentence}."
+    return sentence
+
+
 run_tests()
 
 doctest.testmod()
-
-# TODO: 4. Fix the failing is_long_word function
-# (Don't change the tests, change the function!)
-
-# TODO: 5. Write and test a function to format a phrase as a sentence,
-# starting with a capital and ending with a single full stop.
-# Important: start with a function header and just use pass as the body
-# then add doctests for 3 tests:
-#   'hello' -> 'Hello.'
-#   'It is an ex parrot.' -> 'It is an ex parrot.'
-# and one more that you decide is a useful test.
-# Run your doctests and watch the tests fail.
-# Then write the body of the function so that the tests pass.
