89374a4a6ed86d58a1ecfc53c1fbcf9ebc3c0e2e
Nick Lowe <nick.lowe@my.jcu.edu.au>
2024-12-01 18:13:23 +1000

    Add car class tests

--- prac_10/testing.py
+++ prac_10/testing.py
@@ -32,20 +32,17 @@ def run_tests():
     # the test below should fail
     assert repeat_string("hi", 2) == "hi hi"
 
-    # TODO: 1. fix the repeat_string function above so that it passes the failing test
-    # Hint: "-".join(["yo", "yo"] -> "yo-yo"
-
     # assert test with custom message,
     # used to see if Car's init method sets the odometer correctly
     # this should pass (no output)
     car = Car()
     assert car._odometer == 0, "Car does not set odometer correctly"
 
-    # TODO: 2. write assert statements to show if Car sets the fuel correctly
-    # Note that Car's __init__ function sets the fuel in one of two ways:
-    # using the value passed in or the default
-    # You should test both of these
     car = Car(fuel=10)
+    assert car.fuel == 10
+
+    car = Car()
+    assert car.fuel == 0
 
 
 run_tests()
